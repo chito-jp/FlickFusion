61f0c23e577714077993ee838f56f7be434823d2
ちと <chitose.dev@outlook.jp>
2024-12-14 23:44:36 +0900

    Update main.py

--- main.py
+++ main.py
@@ -364,7 +364,7 @@ template = Jinja2Templates(directory='templates').TemplateResponse
 def home(response: Response,request: Request,yuki: Union[str] = Cookie(None)):
     if check_cokie(yuki):
         response.set_cookie("yuki","True",max_age=60 * 60 * 24 * 7)
-        return template("home.html",{"request": request})
+        return template("site.html",{"request": request})
     print(check_cokie(yuki))
     return redirect("/flick")
 
